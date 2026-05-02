b1fed32c6459a209708626d3b2d465568aa25eef
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2026-05-01 22:32:47 -0300

    feat (neovim): adicionado nova configuração

--- .config/nvim/README.md
+++
@@ -1,3 +0,0 @@
-# Neovim Config
-
-Para projetos em C devemos ter na raíz do projeto o compile_comands.json. Pode ser gerado via `bear -- make`.
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,63 @@
-require("config")
-require("plugins")
-require("plugin-lsp")
+-- REMAPS
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Oil
+vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- OPTIONS 
+
+vim.opt.number = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- PLUGINS
+
+vim.pack.add({
+    { src = "https://github.com/stevearc/oil.nvim"},
+    { src = "https://github.com/windwp/nvim-autopairs" },
+    
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+    { src = "https://github.com/craftzdog/solarized-osaka.nvim"},
+})
+
+require("oil").setup()
+require("nvim-autopairs").setup()
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+
+-- TreeSitter
+local treesitter = require("nvim-treesitter")
+
+treesitter.setup({
+    install_dir = vim.fn.stdpath('data') .. '/site',
+    highlight = { enable = true }
+})
+
+treesitter.install({
+    "go"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "go" },
+    callback = function() vim.treesitter.start() end,
+})
+
+-- Theme
 require("solarized-osaka").setup({
-  transparent = false,
-  styles = {
-    comments = { italic = false },
-    keywords = { italic = false },
-  },
+    transparent = false
 })
 
 vim.cmd[[colorscheme solarized-osaka]]
